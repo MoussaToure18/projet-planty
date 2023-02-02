415ce47c4d7d7d2787df69c6523dd6fd80ab1934
Moussa Toure <aboualyasandy@gmail.com>
2023-02-02 08:52:28 +0100

    CHANGEMENT

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -5,3 +5,12 @@ function theme_enqueue_styles()
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 }
+
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu', 1, 2);
+function add_extra_item_to_nav_menu($items, $args)
+{
+    if (is_user_logged_in()) {
+        $items .= '<li><a href="' . get_permalink(get_option('woocommerce_myaccount_page_id')) . '">My Account</a></li>';
+    }
+    return $items;
+}
